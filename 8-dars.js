5c0bae5e8058d8dcd1d7e53ff4bd55fbe99394f0
alisher-toxirov <alishertoxirov1314@gmail.com>
2026-01-22 19:15:34 +0500

    my-work

---
+++ 8-dars.js
@@ -0,0 +1,44 @@
+// function  logHello() {
+//     console.log('Hello');
+//     console.log('what');
+
+// }
+// logHello()
+
+
+// function  message(message , count = 2) {
+//    for(let a = 0; a < count ; a++){
+//     console.log(message)
+//    }
+// }
+// message('Alisher' ,5)
+// message('Alisher')
+
+
+
+// function sum(a,b){
+//     return a+b
+
+//     // console.log('privet')
+//     // console.log('privet') = ne rabotaet
+    
+
+// }
+// console.log(sum(111,112));
+
+
+
+// function getAgeType(age) {
+
+//     if ( typeof age !== 'number') {
+//         return 'Возраст укозон неконкретно'
+//     }
+//     if (age < 1 || age > 99) {
+//         return 'такого возраста не может'
+//     }
+//     if ( age <18 ){
+//         return 'Несовершеннолетний'
+//     }
+//     return 'Взрослый'
+// }
+// console.log(getAgeType(19));
